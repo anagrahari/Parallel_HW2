3b41270e6143a971958ea5b897bd86ef63f6f599
mukul <mukul312@gmail.com>
2017-04-15 03:04:08 -0500

    dsds

--- par_deterministic_cc.cpp
+++ par_deterministic_cc.cpp
@@ -58,35 +58,52 @@ int par_sum(vector<int> &X, int n) {
 }
 
 
-vector<int> par_deterministic_cc(int n, vector<Edge> &e, vector<int> L, int m){
-	if (m == 0) return L;
+vector<int> par_deterministic_cc(int n, vector<Edge> &e, vector<int> L, int m) {
+	if (m == 0) 
+		return L;
 	vector<int> L2H(n+1);
 	vector<int> H2L(n+1);
 	vector<int> S(m+1);
-	cilk_for (int v = 1; v <= n; v++){
+
+	cilk_for (int v = 1; v <= n; v++) { 
 		L2H[v] = 0;
 		H2L[v] = 0;
 	}
-	cilk_for (int i = 1; i <= m; i++){
-		if (E[i].u < E[i].v) L2H[E[i].u] = 1;
-		else H2L[E[i].u] = 1;
+
+	cilk_for (int i = 1; i <= m; i++) {
+		if (E[i].u < E[i].v) 
+			L2H[E[i].u] = 1;
+		else 
+			H2L[E[i].u] = 1;
 	}
+
 	int n1 = par_sum(L2H, n);
 	int n2 = par_sum(H2L, n);
-	cilk_for (int i = 1; i <= m; i++){
-		if (n1 >= n2 && E[i].u < E[i].v) L[E[i].u] = E[i].v;
-		else if (n1 >= n2 && E[i].v < E[i].u) L[E[i].v] = E[i].u;
-		else if (n1 < n2 && E[i].u > E[i].v) L[E[i].u] = E[i].v;
-		else if (n1 < n2 && E[i].v > E[i].u) L[E[i].v] = E[i].u;
+
+	cilk_for (int i = 1; i <= m; i++) {
+		if (n1 >= n2) {
+			if (E[i].u < E[i].v)
+				L[E[i].u] = E[i].v;
+			else if (E[i].v < E[i].u)
+				L[E[i].v] = E[i].u;
+		} else {
+			if (E[i].u > E[i].v)
+				L[E[i].u] = E[i].v;
+			else if (E[i].v > E[i].u)
+				L[E[i].v] = E[i].u;
+		}
 	}
+
 	find_roots(n,L,L);
-	cilk_for (int i = 1; i <= m; i++){
+
+	cilk_for (int i = 1; i <= m; i++) {
 		S[i] = (L[E[i].u] != L[E[i].v]) ? 1 : 0;
 	}
+
 	S = par_prefix_sum(S, m);
 	vector<Edge> F(S[m]+1);
-	cilk_for (int i =1; i <= m; i++){
-		if (L[E[i].u] != L[E[i].v]){
+	cilk_for (int i =1; i <= m; i++) {
+		if (L[E[i].u] != L[E[i].v]) {
 			F[S[i]].u = L[E[i].u];
 			F[S[i]].v = L[E[i].v];
 		}
@@ -99,9 +116,9 @@ int main() {
 	struct timeval start,end;
 	
 	initialize();
-	gettimeofday(&start,NULL); //Start timing the computation
+	gettimeofday(&start, NULL); //Start timing the computation
 	L = par_deterministic_cc(N, E, L, M);
-	gettimeofday(&end,NULL); //Stop timing the computation
+	gettimeofday(&end, NULL); //Stop timing the computation
 	double myTime = (end.tv_sec+(double)end.tv_usec/1000000) -
 			 (start.tv_sec+(double)start.tv_usec/1000000);
 	cout << "Time taken by algorithm: " << myTime << " seconds.\n";
